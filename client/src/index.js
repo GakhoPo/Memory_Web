626b664bb88fd12dedb8ff8ae9dda6355629e21a
JadenPo <54308076+JadenPo@users.noreply.github.com>
2021-07-14 16:44:50 +0800

    files

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,4 @@
-import React, { PureComponent } from "react";
-import ReactDom from "react-dom";
-
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
-
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- server/index.js
+++ server/index.js
@@ -3,7 +3,29 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+    "mongodb+srv://GakhoPo:GakhoPo123@gakhopo.vmgdh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(PORT, () => {
+            console.log(`server is running on Port: ${PORT}`);
+        })
+    )
+    .catch((error) => console.log(error));
+
+mongoose.set("useFindAndModify", false);
